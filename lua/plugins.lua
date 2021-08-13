31ed22e679ac0832830bc0251dd17a390278ec80
Vinh <xvp351@outlook.com>
2021-08-13 17:10:02 +1000

    Fixed tabbing issue

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -14,4 +14,6 @@ return require('packer').startup(function()
     use 'kyazdani42/nvim-tree.lua'
     use 'neovim/nvim-lspconfig'
     use 'hrsh7th/nvim-compe'
+    use 'hrsh7th/vim-vsnip'
+    use 'hrsh7th/vim-vsnip-integ'
 end)
