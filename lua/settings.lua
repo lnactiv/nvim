846589027289d9001c77f09e0d4813d1632e3bd1
Vinh <xvp351@outlook.com>
2021-08-13 17:11:57 +1000

    Added basic vim settings

---
+++ lua/settings.lua
@@ -0,0 +1,11 @@
+vim.o.autoindent = true
+vim.o.expandtab = true
+vim.o.shiftround = true
+vim.o.shiftwidth = 4
+vim.o.smarttab = true
+vim.o.tabstop = 4
+vim.o.number = true
+vim.o.mouse = 'a'
+vim.o.relativenumber = true
+vim.o.syntax = 'on'
+
