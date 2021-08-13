f656f779da91a3f0181e84f71237e7dbe863d361
Vinh <xvp351@outlook.com>
2021-08-13 17:13:12 +1000

    Can toggle relative line numbers

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -21,4 +21,5 @@ vim.api.nvim_set_keymap('v', '>', '>gv', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('x', 'K', ':move \'<-2<CR>gv-gv', { noremap = true, silent = true})
 vim.api.nvim_set_keymap('x', 'J', ':move \'>+1<CR>gv-gv', { noremap = true, silent = true})
 
-
+-- toggle relative line numbers
+vim.api.nvim_set_keymap('n', '<Leader>l', ':set invrelativenumber<CR>', { noremap = true, silent = true})
