20a381539955efae76a2f1dd1654b6b9ce5403a9
Vinh <xvp351@outlook.com>
2021-08-13 15:39:55 +1000

    Can shift text up and down

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -17,3 +17,8 @@ vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true 
 vim.api.nvim_set_keymap('v', '<', '<gv', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '>', '>gv', { noremap = true, silent = true })
 
+-- move selected line / block of text in visual mode
+vim.api.nvim_set_keymap('x', 'K', ':move \'<-2<CR>gv-gv', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('x', 'J', ':move \'>+1<CR>gv-gv', { noremap = true, silent = true})
+
+
