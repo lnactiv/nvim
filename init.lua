30e09148262ed85b3a650cdfaa72c20a257aa9e2
Vinh <xvp351@outlook.com>
2021-08-13 18:29:17 +1000

    Added c, c++ ls

--- init.lua
+++ init.lua
@@ -1,7 +1,10 @@
 require('plugins')
 require('keymappings')
 require('nv-compe')
+
 require('python-lsp')
+require('clangd-lsp')
+
 
 vim.g.onedark_transparent_background = true
 require('onedark').setup()
